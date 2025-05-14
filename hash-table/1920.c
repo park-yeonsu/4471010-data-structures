4002b567ea3580cbfb0c2af30a208ecaa0db12c3
Yeonsu Park <yeonsu.park.cs@gmail.com>
2025-05-15 01:16:17 +0900

    Update 1920.c

--- hash-table/1920.c
+++ hash-table/1920.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
-#define kTableSize 100003
+#define kInitialCapacity 100003
+#define kLoadFactorThreshold 0.75
 
 typedef struct Node {
     int key;
@@ -10,11 +10,42 @@ typedef struct Node {
 } Node;
 
 typedef struct HashTable {
-    Node* buckets[kTableSize];
+    Node** buckets;
+    int capacity;
+    int size;
 } HashTable;
 
-int Hash(int key) {
-    return (key % kTableSize + kTableSize) % kTableSize;
+int Hash(int key, int capacity) {
+    return (key % capacity + capacity) % capacity;
+}
+
+void ResizeHashTable(HashTable* table) {
+    int oldCapacity = table->capacity;
+    Node** oldBuckets = table->buckets;
+
+    table->capacity = 2 * oldCapacity;
+
+    table->buckets = (Node**)calloc(table->capacity, sizeof(Node*));
+    if (table->buckets == NULL) {
+        table->buckets = oldBuckets; 
+        table->capacity = oldCapacity;
+        return; 
+    }
+
+    for (int i = 0; i < oldCapacity; i++) {
+        Node* current = oldBuckets[i];
+        while (current != NULL) {
+            Node* nodeToRehash = current;
+            current = current->next; 
+
+            int newIndex = Hash(nodeToRehash->key, table->capacity);
+            
+            nodeToRehash->next = table->buckets[newIndex];
+            table->buckets[newIndex] = nodeToRehash;
+        }
+    }
+    
+    free(oldBuckets);
 }
 
 Node* CreateNode(int key) {
@@ -27,18 +58,26 @@ Node* CreateNode(int key) {
     return newNode;
 }
 
-HashTable CreateHashTable() {
-    HashTable table;
+HashTable* CreateHashTable() {
+    HashTable* table = (HashTable*)malloc(sizeof(HashTable));
+    if (table == NULL) {
+        exit(1);
+    }
+
+    table->capacity = kInitialCapacity;
+    table->size = 0;
     
-    for (int i = 0; i < kTableSize; i++) {
-        table.buckets[i] = NULL;
+    table->buckets = (Node**)calloc(table->capacity, sizeof(Node*));
+    if (table->buckets == NULL) {
+        free(table);
+        exit(1);
     }
     
     return table;
 }
 
 void Insert(HashTable* table, int key) {
-    int index = Hash(key);
+    int index = Hash(key, table->capacity);
     
     Node* current = table->buckets[index];
     while (current != NULL) {
@@ -51,10 +90,15 @@ void Insert(HashTable* table, int key) {
     Node* newNode = CreateNode(key);
     newNode->next = table->buckets[index];
     table->buckets[index] = newNode;
+    table->size++;
+
+    if ((double)table->size / table->capacity > kLoadFactorThreshold) {
+        ResizeHashTable(table);
+    }
 }
 
 int Search(HashTable* table, int key) {
-    int index = Hash(key);
+    int index = Hash(key, table->capacity);
     
     Node* current = table->buckets[index];
     while (current != NULL) {
@@ -68,35 +112,38 @@ int Search(HashTable* table, int key) {
 }
 
 void FreeHashTable(HashTable* table) {
-    for (int i = 0; i < kTableSize; i++) {
+    if (table == NULL) return;
+
+    for (int i = 0; i < table->capacity; i++) {
         Node* current = table->buckets[i];
         while (current != NULL) {
             Node* temp = current;
             current = current->next;
             free(temp);
         }
-        table->buckets[i] = NULL;
     }
+    free(table->buckets);
+    free(table);
 }
 
 int main() {
     int N, M;
-    HashTable table = CreateHashTable();
+    HashTable* table = CreateHashTable();
     
     scanf("%d", &N);
     for (int i = 0; i < N; i++) {
         int num;
         scanf("%d", &num);
-        Insert(&table, num);
+        Insert(table, num);
     }
     
     scanf("%d", &M);
     for (int i = 0; i < M; i++) {
         int num;
         scanf("%d", &num);
-        printf("%d\n", Search(&table, num));
+        printf("%d\n", Search(table, num));
     }
     
-    FreeHashTable(&table);
+    FreeHashTable(table);
     return 0;
 }
